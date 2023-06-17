a8f9fe2cb8826ef0c4e8182a82104b114ba87f36
Josué Makuta <makutajosue@gmail.com>
2023-06-17 19:19:52 +0200

    Main b

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,7 +7,7 @@ const HomeBlog = () => {
   return (
     <Menu>
       <main className="flex justify-center bg-[#f0f0f0] flex-col animate-background items-center dark:text-default_color dark:bg-dark_bg dark:bg-main ">
-        Welcome to my blog !
+        Home blog ! Happy to see you on my blog ...
       </main>
     </Menu>
   );
